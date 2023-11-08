140fbfa1da60b2f349c0f8c7593ca25d4af98d27
darshanlal2k <darshanlal2k@gmail.com>
2023-11-09 02:53:26 +0530

    typescript any

---
+++ 9-any/script.js
@@ -0,0 +1,9 @@
+var something = "Hello World!";
+console.log(something);
+something = 23;
+console.log(something);
+something = true;
+console.log(something);
+var arr = ["John", 212, true];
+arr.push("Smith");
+console.log(arr); //Output: [ 'John', 212, true, 'Smith' ] 
---
+++ 9-any/script.ts
@@ -0,0 +1,13 @@
+let something: any = "Hello World!"; 
+console.log(something);
+something = 23;
+console.log(something);
+something = true;
+console.log(something);
+
+
+
+let arr: any[] = ["John", 212, true]; 
+arr.push("Smith"); 
+console.log(arr); //Output: [ 'John', 212, true, 'Smith' ] 
+
