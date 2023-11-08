6086262ed383e5489f4a6fd22042cbc790fe0fd6
darshanlal2k <darshanlal2k@gmail.com>
2023-11-09 02:50:59 +0530

    typescript String

---
+++ 3-String/string.js
@@ -0,0 +1,29 @@
+var darshan = 'lal';
+//OR
+var darshanlla = "lal";
+// pre and post es6
+var fun = "John Smith";
+var jolly = "Finance";
+var preES = fun + " works in the " + jolly + " department.";
+var postES = "".concat(fun, " works in the ").concat(jolly, " department.");
+console.log(preES);
+console.log(postES);
+//charAT()
+var str = 'Hello TypeScript';
+str.charAt(0);
+str.charAt(2);
+console.log("Hello World".charAt(2));
+// indexof()
+var sample = 'visual studio code';
+console.log(sample.indexOf('v'));
+console.log(sample.indexOf('s'));
+console.log(sample.indexOf('c'));
+console.log(sample.indexOf('l'));
+console.log(sample.indexOf('u'));
+console.log(sample.indexOf('i', 2));
+//toupppercase()
+var simpu = 'typescript';
+console.log(simpu.toUpperCase());
+//to lowercase()
+var VIJAY = 'JAVASCRIPT';
+console.log(VIJAY.toLowerCase());
---
+++ 3-String/string.ts
@@ -0,0 +1,53 @@
+
+
+let darshan:string = 'lal'; 
+//OR
+let darshanlla:string = "lal"; 
+
+
+// pre and post es6
+
+let fun:string = "John Smith"; 
+let jolly:string = "Finance"; 
+
+
+let preES: string = fun + " works in the " + jolly + " department."; 
+
+ 
+let postES: string = `${fun} works in the ${jolly} department.`; 
+
+console.log(preES);
+console.log(postES);
+
+
+//charAT()
+let str: string = 'Hello TypeScript';
+str.charAt(0); 
+str.charAt(2); 
+
+console.log ("Hello World".charAt(2));
+
+// indexof()
+
+let sample: string = 'visual studio code';
+
+console.log(sample.indexOf('v'));
+console.log(sample.indexOf('s'));
+console.log(sample.indexOf('c'));
+console.log(sample.indexOf('l'));
+console.log(sample.indexOf('u'));
+console.log(sample.indexOf('i',2));
+
+
+//toupppercase()
+
+let simpu: string = 'typescript';
+
+console.log(simpu.toUpperCase());
+
+//to lowercase()
+
+let VIJAY: string = 'JAVASCRIPT';
+
+console.log(VIJAY.toLowerCase());
+
