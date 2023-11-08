6f25f537cb8e4f3f51eea07baa227d797572583f
darshanlal2k <darshanlal2k@gmail.com>
2023-11-09 02:51:26 +0530

    typescript Boolean

---
+++ 4-Boolean/boolean.js
@@ -0,0 +1,8 @@
+var a = true;
+var b = false;
+var isVerified = true;
+var isDone = false;
+console.log(isVerified); // true
+console.log(isDone); // false
+console.log(a);
+console.log(b);
---
+++ 4-Boolean/boolean.ts
@@ -0,0 +1,13 @@
+let a :boolean = true;
+
+let b:boolean = false;
+var isVerified: boolean = true;
+
+var isDone: Boolean = false;
+
+console.log(isVerified); // true
+
+console.log(isDone); // false
+
+console.log(a);
+console.log(b);
