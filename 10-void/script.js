519f9ee228c5ea70f5a5158d85bb310cfce72507
darshanlal2k <darshanlal2k@gmail.com>
2023-11-09 02:53:44 +0530

    typescript void

---
+++ 10-void/script.js
@@ -0,0 +1,9 @@
+function sayHi() {
+    console.log('Hi!');
+}
+var speech = sayHi();
+console.log(speech); //Output: undefined
+var nothing = undefined;
+console.log(nothing);
+var vishnu = 1;
+console.log(vishnu); // Error
---
+++ 10-void/script.ts
@@ -0,0 +1,13 @@
+function sayHi(): void { 
+    console.log('Hi!')
+} 
+
+let speech: void = sayHi(); 
+console.log(speech); //Output: undefined
+
+let nothing: void = undefined;
+console.log(nothing);
+
+let vishnu: void = 1;
+console.log(vishnu); // Error
+
