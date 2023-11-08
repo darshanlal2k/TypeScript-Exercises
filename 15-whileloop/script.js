3174526fb9df9124d3bca7758822ae1e5da0ce29
darshanlal2k <darshanlal2k@gmail.com>
2023-11-09 02:56:33 +0530

    typescript while loop

---
+++ 15-whileloop/script.js
@@ -0,0 +1,10 @@
+var i = 2;
+while (i < 4) {
+    console.log("Block statement execution no." + i);
+    i++;
+}
+var ii = 2;
+do {
+    console.log("Block statement execution no." + ii);
+    ii++;
+} while (ii < 4);
---
+++ 15-whileloop/script.ts
@@ -0,0 +1,16 @@
+
+let i: number = 2;
+
+while (i < 4) {
+    console.log( "Block statement execution no." + i )
+    i++;
+}
+
+
+let ii: number = 2;
+do {
+    console.log("Block statement execution no." + ii )
+    ii++;
+} while ( ii < 4)
+
+
